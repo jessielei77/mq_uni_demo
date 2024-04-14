d8f4cfad3c379fcbebbe4cc727a170719f0fafa5
jessielei77 <165538158+jessielei77@users.noreply.github.com>
2024-04-14 15:16:53 +1000

    Update adhoc_query.sql

--- analyses/adhoc_query.sql
+++ analyses/adhoc_query.sql
@@ -1,11 +1,12 @@
 select
     unit_title,
     enrolment_status,
-    count(distinct student_key) as number_of_students
+    count(distinct student_id) as number_of_students
 from
-    {{ ref('fact_student_unit_enrolment') }} fsue
+    {{ ref('fact_student_unit_enrolment_scd2') }} fsue
     inner join
-    {{ ref('dim_unit') }} u
-    on fsue.unit_key = u.unit_key
+    {{ ref('dim_unit_scd2') }} u
+    on fsue.unit_id = u.unit_id
+    and fsue.enrolment_date_key between u.dbt_valid_from and coalesce(u.dbt_valid_to,'2999-12-31')
     group by     unit_title,
     enrolment_status;
